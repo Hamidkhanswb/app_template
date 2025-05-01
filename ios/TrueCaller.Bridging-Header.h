ad48688d7be9e2234c956357db0c33fadb3c9114
hamid <hamid@hamids-MacBook-Air.local>
2025-05-01 00:35:26 -0700

    basic template

---
+++ ios/TrueCaller.Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TrueCaller.Bridging-Header.h
+//  TrueCaller
+//
+//  Created by hamid on 30/04/2025.
+//
+#import "RNSplashScreen.h
+
+#ifndef TrueCaller_Bridging_Header_h
+#define TrueCaller_Bridging_Header_h
+
+
+#endif /* TrueCaller_Bridging_Header_h */
